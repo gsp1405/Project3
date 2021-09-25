7e066913c508cd70926711531a24dd853c957477
Czelucha <90610175+Czelucha@users.noreply.github.com>
2021-09-25 20:11:04 +0200

    task2

--- twoChanIm.m
+++ twoChanIm.m
@@ -1,8 +1,5 @@
 function image = twoChanIm(red, green)
 %Initialize the RGB image and visualization:
-if size(red) == size(green)
-    fprintf('size of images are not the same')
-end
 image = zeros ( size (red, 1) , size (red, 2) , 3 );
 image (: ,: ,1) = red; % Red Channel
 image (: ,: ,2) = green; %green channel
